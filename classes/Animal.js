f7f42c1b77eb70107038c0534b2842eed995e835
amny.inc <amny.inc@gmail.com>
2024-01-06 22:05:51 +0100

    inheritance in js

--- classes/Animal.js
+++ classes/Animal.js
@@ -18,7 +18,28 @@ class Animal {
     }
 }
 
-const bobby = new Animal("bobby", 2);
-bobby.eat();
-bobby.sleep();
-bobby.logAge();
+
+
+class Dog extends Animal {
+    constructor(name, age, breed) {
+        super(name, age);
+        this.breed = breed;
+    }
+
+    logBreed() {
+        console.log(`${this.breed}`);
+    }
+
+    logAgeFromAnimal() {
+        super.logAge();
+    }
+}
+
+
+
+
+const mike = new Dog("mike", 3, "bulldog");
+mike.eat();
+mike.sleep();
+mike.logAgeFromAnimal();
+mike.logBreed();
